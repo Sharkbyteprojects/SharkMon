a65878915627333159b4797c278e37ae4b0a6f02
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2020-06-01 13:12:53 +0200

    use babel.js, add deps to Readme, add icons to readme

--- Readme.md
+++ Readme.md
@@ -11,3 +11,20 @@ to the `index.js`
 ### View
 - Open [`http://localhost:8080`](http://localhost:8080)
 in your Browser
+
+## Dependencies
+name 		| Version
+----------- | ---------
+express 	| ^4.17.1
+rxjs 		| ^6.5.5
+socket.io 	| ^2.3.0
+
+## USED ICONS
+### Created by Sharkbyteprojects
+- ![](https://raw.githubusercontent.com/Sharkbyteprojects/SharkMon/master/static/icons/core.svg)
+- ![](https://raw.githubusercontent.com/Sharkbyteprojects/SharkMon/master/static/icons/ram.svg)
+- ![](https://raw.githubusercontent.com/Sharkbyteprojects/SharkMon/master/static/icons/os/osapple.svg)
+- ![](https://raw.githubusercontent.com/Sharkbyteprojects/SharkMon/master/static/icons/os/oslinux.svg)
+- ![](https://raw.githubusercontent.com/Sharkbyteprojects/SharkMon/master/static/icons/os/osms.svg)
+- ![](https://raw.githubusercontent.com/Sharkbyteprojects/SharkMon/master/static/icons/os/osunkown.svg)
+- ![](https://raw.githubusercontent.com/Sharkbyteprojects/SharkMon/master/static/icons/network.svg)
--- static/main.js
+++ static/main.js
@@ -4,13 +4,16 @@
     linux: "oslinux",
     win32: "osms",
     darwin: "osapple",
-    unkown: "osunkown",
+    unkown: "osunkown"
   };
+
   function mkstr(st) {
     return `/icons/os/${st}.svg`;
   }
+
   function os(osname) {
     var x;
+
     if (osname == "linux") {
       x = mkstr(osselect.linux);
     } else if (osname == "win32") {
@@ -20,36 +23,28 @@
     } else {
       x = mkstr(osselect.unkown);
     }
+
     return x;
   }
+
   $(document).ready(() => {
-    $.get(
-      "/api/static",
-      (data) => {
-        $("object.os").attr("data", os(data.os));
-        $("td.os").html(
-          `<li>Arch: ${data.arch}</li><li>Homedir: ${data.homedir}</li><li>Hostname: ${data.hostname}</li><li>Release of OS: ${data.codename}</li><li>Temp Dir: ${data.tmpdir}</li><li class="uptime">OS Uptime: ${data.osuptime} seconds</li><li>OS Name: ${data.os}</li>`
-        );
-        $("strong.mem").html(`Ram: ${data.totalmem} MB`);
-        $("div.mem").html(`Free Ram: ${data.freemem_mb} MB`);
-        socket.on("freemem", function (msg) {
-          $("div.mem").html(`Free Ram: ${msg} MB`);
-        });
-        socket.on("ut", function (msg) {
-            $("li.uptime").html(`OS Uptime: ${msg} seconds`);
-          });
-        for (var cpu of data.cpus) {
-          $("div.core").append(
-            `<li>Cpu Model: ${cpu.model}<br>Speed: ${cpu.speed}MHz</li><br>`
-          );
-        }
-    
-            $("div.net").text(
-              `${JSON.stringify(data.network)}`
-            );
-         
-      },
-      "json"
-    );
+    $.get("/api/static", data => {
+      $("object.os").attr("data", os(data.os));
+      $("td.os").html(`<li>Arch: ${data.arch}</li><li>Homedir: ${data.homedir}</li><li>Hostname: ${data.hostname}</li><li>Release of OS: ${data.codename}</li><li>Temp Dir: ${data.tmpdir}</li><li class="uptime">OS Uptime: ${data.osuptime} seconds</li><li>OS Name: ${data.os}</li>`);
+      $("strong.mem").html(`Ram: ${data.totalmem} MB`);
+      $("div.mem").html(`Free Ram: ${data.freemem_mb} MB`);
+      socket.on("freemem", function (msg) {
+        $("div.mem").html(`Free Ram: ${msg} MB`);
+      });
+      socket.on("ut", function (msg) {
+        $("li.uptime").html(`OS Uptime: ${msg} seconds`);
+      });
+
+      for (var cpu of data.cpus) {
+        $("div.core").append(`<li>Cpu Model: ${cpu.model}<br>Speed: ${cpu.speed}MHz</li><br>`);
+      }
+
+      $("div.net").text(`${JSON.stringify(data.network)}`);
+    }, "json");
   });
 })();
