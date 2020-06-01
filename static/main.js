5a4bb2b49b3ab6f3db5a0a9f115d2fb25e271abd
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2020-06-01 08:45:53 +0200

    ADD OS ICONS

--- static/main.js
+++ static/main.js
@@ -1,8 +1,12 @@
 (function () {
   const socket = io();
   $(document).ready(() => {
+      $.get("/api/static",(data)=>{
+          $("strong.mem").html(`Ram: ${data.totalmem} MB`);
+          $("div.mem").html(`Free Ram: ${data.freemem_mb} MB`);
     socket.on("freemem", function (msg) {
-      $("#messages").append($("<li>").text(msg));
+        $("div.mem").html(`Free Ram: ${msg} MB`);
     });
+},"json");
   });
 })();
