281dad4a15d6d2c21efdcb8bff45a73bfcf7a8b1
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2020-06-02 07:16:20 +0200

    run eslint with fix on index.js

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const expr = require("express");
-const app = expr();
-const get = { static: require("./get/static"), dyn: require("./get/dynamic") };
-app.use(expr.static(__dirname+"/static"));
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/static/index.html");
-});
-app.get("/api/static", (req, res) => {
-  res.jsonp(get.static());
-});
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-let numberOfUsers = 0;
-let freemem = get.static().freemem_mb;
-function resock() {
-  process.stdout.write("\r");
-  process.stdout.write(`Users: ${numberOfUsers} || Free Memory: ${freemem} MB`);
+const expr = require('express')
+const app = expr()
+const get = { static: require('./get/static'), dyn: require('./get/dynamic') }
+app.use(expr.static(path.join(__dirname, 'static')))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/static/index.html')
+})
+app.get('/api/static', (req, res) => {
+  res.jsonp(get.static())
+})
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+let numberOfUsers = 0
+let freemem = get.static().freemem_mb
+function resock () {
+  process.stdout.write('\r')
+  process.stdout.write(`Users: ${numberOfUsers} || Free Memory: ${freemem} MB`)
 }
-io.on("connection", (socket) => {
-  numberOfUsers++;
-  resock();
-  socket.on("disconnect", () => {
-    numberOfUsers--;
-    resock();
-  });
-});
+io.on('connection', (socket) => {
+  numberOfUsers++
+  resock()
+  socket.on('disconnect', () => {
+    numberOfUsers--
+    resock()
+  })
+})
 get.dyn.mem.subscribe((data) => {
-  io.emit("freemem", data);
-  freemem = data;
-  resock();
-});
-get.dyn.upt.subscribe((data)=>{
-    io.emit("ut",data);
-});
+  io.emit('freemem', data)
+  freemem = data
+  resock()
+})
+get.dyn.upt.subscribe((data) => {
+  io.emit('ut', data)
+})
 http.listen(process.env.PORT || 8080, () => {
-  console.log("Listen on http://localhost:8080");
-});
+  console.log('Listen on http://localhost:8080')
+})
