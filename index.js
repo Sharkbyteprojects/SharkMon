71210d58b6a9d19f44cfc9d3fc1b63f7373473e4
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2020-06-22 21:30:59 +0200

    --fix Eslint

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
-const expr = require("express");
-const app = expr();
-const path = require("path");
-const get = { static: require("./get/static"), dyn: require("./get/dynamic") };
-app.use(expr.static(path.join(__dirname, "static")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "index.html"));
-});
-app.get("/api/static", (req, res) => {
-  res.jsonp(get.static());
-});
-const http = require("http").createServer(app);
-const io = require("socket.io")(http).of("/api/dynamic");
-let numberOfUsers = 0;
-let freemem = get.static().freemem_mb;
-let nerrl = true;
-function resock() {
+const expr = require('express')
+const app = expr()
+const path = require('path')
+const get = { static: require('./get/static'), dyn: require('./get/dynamic') }
+app.use(expr.static(path.join(__dirname, 'static')))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'index.html'))
+})
+app.get('/api/static', (req, res) => {
+  res.jsonp(get.static())
+})
+const http = require('http').createServer(app)
+const io = require('socket.io')(http).of('/api/dynamic')
+let numberOfUsers = 0
+let freemem = get.static().freemem_mb
+let nerrl = true
+function resock () {
   if (nerrl) {
     try {
-      process.stdout.clearLine();
-      process.stdout.cursorTo(0);
+      process.stdout.clearLine()
+      process.stdout.cursorTo(0)
     } catch (e) {
-      nerrl = false;
-      process.stdout.write("\r");
+      nerrl = false
+      process.stdout.write('\r')
     }
   } else {
-    process.stdout.write("\r");
+    process.stdout.write('\r')
   }
-  process.stdout.write(`Users: ${numberOfUsers} || Free Memory: ${freemem} MB`);
+  process.stdout.write(`Users: ${numberOfUsers} || Free Memory: ${freemem} MB`)
 }
 
-io.on("connection", (socket) => {
-  numberOfUsers++;
-  resock();
-  socket.on("disconnect", () => {
-    numberOfUsers--;
-    resock();
-  });
-});
+io.on('connection', (socket) => {
+  numberOfUsers++
+  resock()
+  socket.on('disconnect', () => {
+    numberOfUsers--
+    resock()
+  })
+})
 get.dyn.mem.subscribe((data) => {
-  io.emit("freemem", data);
-  freemem = data;
-  resock();
-});
+  io.emit('freemem', data)
+  freemem = data
+  resock()
+})
 get.dyn.upt.subscribe((data) => {
-  io.emit("ut", data);
-});
+  io.emit('ut', data)
+})
 http.listen(process.env.PORT || 8080, () => {
-  console.log("Listen on http://localhost:8080");
-});
+  console.log('Listen on http://localhost:8080')
+})
